063be7787574cc0057dffcb8b6aa1100b30f9582
arnoldgf <arnoldgf@mail.ru>
2024-12-24 22:50:49 +0300

    1.0

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -2,6 +2,9 @@ package hiber.service;
 
 import hiber.dao.UserDao;
 import hiber.model.User;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +16,8 @@ public class UserServiceImp implements UserService {
 
    @Autowired
    private UserDao userDao;
+   @Autowired
+   private SessionFactory sessionFactory;
 
    @Transactional
    @Override
@@ -24,6 +29,17 @@ public class UserServiceImp implements UserService {
    @Override
    public List<User> listUsers() {
       return userDao.listUsers();
+   }
+
+   //Очистка таблицы перед добавлением
+   @Override
+   @Transactional
+   public void clearAllData() {
+      Session session = sessionFactory.getCurrentSession();
+      session.createQuery("delete from User").executeUpdate();
+      session.createQuery("delete from Car").executeUpdate();
+      session.createNativeQuery("ALTER TABLE users AUTO_INCREMENT = 1").executeUpdate();
+      session.createNativeQuery("ALTER TABLE cars AUTO_INCREMENT = 1").executeUpdate();
    }
 
 }
